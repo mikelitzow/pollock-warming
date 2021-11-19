bf8f8a107580df7631156fe3b79e2cd07ad6d91e
KristaOke <kristaoke@gmail.com>
2021-11-19 14:01:28 -0900

    updating models, too few df with interaction term so models are now sex specific

--- scripts/weight-at-age_models.R
+++ scripts/weight-at-age_models.R
@@ -12,6 +12,9 @@ theme_set(theme_bw())
 
 #let's split the data out into age classes
 
+all.dat$sex.code <- as.factor(all.dat$sex.code)
+all.dat$maturity_table_3 <- as.factor(all.dat$maturity_table_3)
+
 #starts at age 4
 age4dat <- all.dat[which(all.dat$Age==4),]
 age5dat <- all.dat[which(all.dat$Age==5),]
@@ -23,89 +26,197 @@ age10dat <- all.dat[which(all.dat$Age==10),]
 
 #models=====================================================================
 #start by trying same models as for EBS
+#these models are using nov-feb SST, will re-do with spring SST too
+
+#NO LAT x LONG in these models because these are sampled during spawning, so fish
+#from various places came together into one big spawning aggregation
+
+#models are rank deficient (seems like too few DF) when both sexes modeled together with a sex : maturity interaction
+#SO we will do age-specific sex-specific models
 
 #age 4 models-------
 
-mod4 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age4dat)
-gam.check(mod4$gam)
-summary(mod4$gam)
-summary(mod4$mer)
+table(age4dat$sex.code, age4dat$maturity_table_3)
 
-plot(mod4$gam)
+#males
+mod4male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                s(julian, k=4) + maturity_table_3,
+              random=~(1|year/Haul), data=age4dat[which(age4dat$sex.code==1),])
+gam.check(mod4male$gam)
+summary(mod4male$gam)
+summary(mod4male$mer)
+
+plot(mod4male$gam)
+
+#females
+mod4fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age4dat[which(age4dat$sex.code==2),])
+gam.check(mod4fem$gam)
+summary(mod4fem$gam)
+summary(mod4fem$mer)
+
+plot(mod4fem$gam)
+
+
+ggplot(age4dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 #age 5 models-------
 
-mod5 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age5dat)
-gam.check(mod5$gam)
-summary(mod5$gam)
-summary(mod5$mer)
+table(age5dat$sex.code, age5dat$maturity_table_3)
 
-plot(mod5$gam)
+#males
+mod5male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age5dat[which(age5dat$sex.code==1),])
+gam.check(mod5male$gam)
+summary(mod5male$gam)
+summary(mod5male$mer)
 
+plot(mod5male$gam)
+
+#females
+mod5fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age5dat[which(age5dat$sex.code==2),])
+gam.check(mod5fem$gam)
+summary(mod5fem$gam)
+summary(mod5fem$mer)
+
+plot(mod5fem$gam)
+
+ggplot(age5dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 #age 6 models-------
 
-mod6 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age6dat)
-gam.check(mod6$gam)
-summary(mod6$gam)
-summary(mod6$mer)
+table(age6dat$sex.code, age6dat$maturity_table_3)
 
-plot(mod6$gam)
+#males
+mod6male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age6dat[which(age6dat$sex.code==1),])
+gam.check(mod6male$gam)
+summary(mod6male$gam)
+summary(mod6male$mer)
 
+plot(mod6male$gam)
+
+#females
+mod6fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age6dat[which(age6dat$sex.code==2),])
+gam.check(mod6fem$gam)
+summary(mod6fem$gam)
+summary(mod6fem$mer)
+
+plot(mod6fem$gam)
+
+ggplot(age6dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 #age 7 models-------
 
-mod7 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age7dat)
-gam.check(mod7$gam)
-summary(mod7$gam)
-summary(mod7$mer)
+table(age7dat$sex.code, age7dat$maturity_table_3)
 
-plot(mod7$gam)
+#males
+mod7male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age7dat[which(age7dat$sex.code==1),])
+gam.check(mod7male$gam)
+summary(mod7male$gam)
+summary(mod7male$mer)
+
+plot(mod7male$gam)
+
+#females
+mod7fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age7dat[which(age7dat$sex.code==2),])
+gam.check(mod7fem$gam)
+summary(mod7fem$gam)
+summary(mod7fem$mer)
+
+plot(mod7fem$gam)
+
+ggplot(age7dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 
 #age 8 models-------
 
-mod8 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age8dat)
-gam.check(mod8$gam)
-summary(mod8$gam)
-summary(mod8$mer)
+table(age8dat$sex.code, age8dat$maturity_table_3)
 
-plot(mod8$gam)
+#males
+mod8male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age8dat[which(age8dat$sex.code==1),])
+gam.check(mod8male$gam)
+summary(mod8male$gam)
+summary(mod8male$mer)
 
+plot(mod8male$gam)
+
+#females
+mod8fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age8dat[which(age8dat$sex.code==2),])
+gam.check(mod8fem$gam)
+summary(mod8fem$gam)
+summary(mod8fem$mer)
+
+plot(mod8fem$gam)
+
+ggplot(age8dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 #age 9 models-------
 
-mod9 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age9dat)
-gam.check(mod9$gam)
-summary(mod9$gam)
-summary(mod9$mer)
+table(age9dat$sex.code, age9dat$maturity_table_3)
 
-plot(mod9$gam)
+#males
+mod9male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age9dat[which(age9dat$sex.code==1),])
+gam.check(mod9male$gam)
+summary(mod9male$gam)
+summary(mod9male$mer)
 
+plot(mod9male$gam)
+
+#females
+mod9fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age9dat[which(age9dat$sex.code==2),])
+gam.check(mod9fem$gam)
+summary(mod9fem$gam)
+summary(mod9fem$mer)
+
+plot(mod9fem$gam)
+
+ggplot(age9dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 #age 10 models-------
 
-mod10 <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + t2(Longitude, Latitude) +
-                s(julian, k=4) + sex.code:maturity_table_3,
-              random=~(1|year/Haul), data=age10dat)
-gam.check(mod10$gam)
-summary(mod10$gam)
-summary(mod10$mer)
+table(age10dat$sex.code, age10dat$maturity_table_3)
 
-plot(mod10$gam)
+#males
+mod10male <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                    s(julian, k=4) + maturity_table_3,
+                  random=~(1|year/Haul), data=age10dat[which(age10dat$sex.code==1),])
+gam.check(mod10male$gam)
+summary(mod10male$gam)
+summary(mod10male$mer)
 
+plot(mod10male$gam)
+
+#females
+mod10fem <- gamm4(sc.weight ~ s(nov.feb.wSST, k=4) + 
+                   s(julian, k=4) + maturity_table_3,
+                 random=~(1|year/Haul), data=age10dat[which(age10dat$sex.code==2),])
+gam.check(mod10fem$gam)
+summary(mod10fem$gam)
+summary(mod10fem$mer)
+
+plot(mod10fem$gam)
+
+ggplot(age10dat, aes(as.factor(maturity_table_3), sc.weight, colour=as.factor(sex.code))) + geom_boxplot()
 
 
 
