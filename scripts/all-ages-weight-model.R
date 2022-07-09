b17340c0c1b2177ff9923dc79e261fea490f00ff
mikelitzow <mike.litzow@noaa.gov>
2022-07-08 22:06:28 -0800

    Removing old models with non-lagged sst data

--- scripts/all-ages-weight-model.R
+++ scripts/all-ages-weight-model.R
@@ -34,57 +34,6 @@ all.dat$sex.code <- as.factor(all.dat$sex.code)
 all.dat$age.factor <- as.factor(all.dat$Age)
 all.dat$maturity_table_3 <- as.factor(all.dat$maturity_table_3)
 
-#big overall model----
-
-#BELOW doesn't make a lot of sense because of timing of survey, skip to the lagged models farther down
-
-# bigmod1 <- gamm4(sc.weight ~ s(annual.wSST, k=4) + s(annual.wSST, by=age.factor, k=4) + #s(annual.wSST, by=sex.code, k=4) +
-#                     maturity_table_3,
-#                   random=~(1|year/Haul), data=all.dat)
-# gam.check(bigmod1$gam)
-# plot(bigmod1$gam)
-# summary(bigmod1$gam)
-# 
-# bigmod2 <- gamm4(sc.weight ~  s(annual.wSST, by=age.factor, k=4), #+ #s(annual.wSST, by=sex.code, k=4),
-#                  random=~(1|year/Haul), data=all.dat)
-# gam.check(bigmod2$gam)
-# plot(bigmod2$gam)
-# summary(bigmod2$gam)
-# anova(bigmod2$gam)
-# 
-# bigmod3 <- gamm4(sc.weight ~  s(annual.wSST, by=age.factor, k=4) + maturity_table_3,
-#                  random=~(1|year/Haul), data=all.dat[which(all.dat$sex.code==1),])
-# gam.check(bigmod3$gam)
-# plot(bigmod3$gam)
-# summary(bigmod3$gam)
-# anova(bigmod3$gam)
-# 
-# bigmod4 <- gamm4(sc.weight ~  s(annual.wSST, by=age.factor, k=4) + maturity_table_3,
-#                  random=~(1|year/Haul), data=all.dat[which(all.dat$sex.code==2),])
-# gam.check(bigmod4$gam)
-# plot(bigmod4$gam)
-# summary(bigmod4$gam)
-# 
-# anova(bigmod4$gam)
-# 
-# bigmodS1 <- gamm4(sc.weight ~ s(annual.wSST, k=4) + age.factor +
-#                    maturity_table_3,
-#                  random=~(1|year/Haul), data=all.dat[which(all.dat$sex.code==1),])
-# gam.check(bigmodS1$gam)
-# plot(bigmodS1$gam)
-# summary(bigmodS1$gam)
-# 
-# bigmodS2 <- gamm4(sc.weight ~ s(annual.wSST, k=4) + age.factor +
-#                     maturity_table_3,
-#                   random=~(1|year/Haul), data=all.dat[which(all.dat$sex.code==2),])
-# gam.check(bigmodS2$gam)
-# plot(bigmodS2$gam)
-# summary(bigmodS2$gam)
-# 
-# AIC(bigmodS1$mer, bigmod3$mer)
-# AIC(bigmodS2$mer, bigmod4$mer)
-
-
 #lag annual sst------
 #reload sst data
 d1 <- read.csv("./data/western.goa.sst.csv")
