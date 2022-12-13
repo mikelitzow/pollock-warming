dc3ea2c4724ba6485852db4ab74e149935ecd0d8
mikelitzow <mike.litzow@noaa.gov>
2022-12-13 07:34:55 -0900

    Adding acoustic trawl data analysis to all_weight script

--- scripts/age_at_weight_all_data_sources.R
+++ scripts/age_at_weight_all_data_sources.R
@@ -157,4 +157,176 @@ new.dat <- data.frame(length = mean(dat$length),
                       sst = seq(min(dat$sst), max(dat$sst), length.out = 100))
 
 
-## now acoustic trawl data
+## now acoustic trawl data ----------------------------------------
+
+
+# first, combine data for different periods into a single df
+
+d1 <- read.csv("./data/ShelikofHistorical_86_94_v2.csv")
+
+d2 <- read.csv("./data/ShelikofHistorical_1995_2002.csv")
+
+d3 <- read.csv("./data/ShelikofHistorical_2003_2020.csv")
+
+d4 <- read.csv("./data/ShelikofHistorical_0704.csv")
+
+# make d1/d2 names compatible
+names(d1)[11] <- "Sex"
+
+# lump into the original maturity schedule (Table 3)
+d1$maturity_table_3 <- d1$Maturity
+
+d2$maturity_table_3 <- ifelse(d2$MaturityTable == 3, d2$Maturity,
+                              ifelse(d2$MaturityTable == 11 & d2$Maturity == 1, 1,
+                                     ifelse(d2$MaturityTable == 11 & d2$Maturity %in% c(2,3), 2,
+                                            ifelse(d2$MaturityTable == 11 & d2$Maturity %in% c(4,5), 3,
+                                                   ifelse(d2$MaturityTable == 11 & d2$Maturity == 6, 4, 5)))))
+d3$maturity_table_3 <- case_when(
+  d3$Maturity == "Immature" ~ 1,
+  d3$Maturity %in% c("Developing I", "Developing II", "Developing") ~ 2,
+  d3$Maturity %in% c("Prespawning I", "Prespawning II", "Prespawning") ~ 3,
+  d3$Maturity == "Spawning" ~ 4,
+  d3$Maturity %in% c("Spent I", "Spent II", "Spent") ~ 5)
+
+d4$maturity_table_3 <- case_when(
+  d4$Maturity == "Immature" ~ 1,
+  d4$Maturity %in% c("Developing I", "Developing II", "Developing") ~ 2,
+  d4$Maturity %in% c("Prespawning I", "Prespawning II", "Prespawning") ~ 3,
+  d4$Maturity == "Spawning" ~ 4,
+  d4$Maturity %in% c("Spent I", "Spent II", "Spent") ~ 5)
+
+# clean up names
+names(d1)[6:7] <- names(d2)[6:7] <- names(d3)[6:7] <- names(d4)[6:7] <- c("length", "weight")
+
+# combine
+d1 <- d1 %>% 
+  select(Survey, Haul, Latitude, Longitude, DateTime, length, weight, Age, Sex, maturity_table_3)
+
+d2 <- d2 %>% 
+  select(Survey, Haul, Latitude, Longitude, DateTime, length, weight, Age, Sex, maturity_table_3)
+
+d3 <- d3 %>% 
+  select(Survey, Haul, Latitude, Longitude, DateTime, length, weight, Age, Sex, maturity_table_3)
+
+d4 <- d4 %>% 
+  select(Survey, Haul, Latitude, Longitude, DateTime, length, weight, Age, Sex, maturity_table_3)
+
+# change d3/d4 weight to g
+d3$weight <- 1000*d3$weight
+d4$weight <- 1000*d4$weight
+
+dat <- rbind(d1, d2, d3, d4)
+
+# get julian day
+dat$julian <- lubridate::yday(temp.more$date)
+dat$year <- lubridate::year(temp.more$date)
+
+# add sex 
+dat$sex.code <- if_else(dat$Sex == 1, 1,
+                        if_else(dat$Sex == 2, 2,
+                                if_else(dat$Sex == "Male", 1, 
+                                        ifelse(dat$Sex == "Female", 2, NA))))
+
+
+# now need to scale weights by age and maturity stage
+
+# first, clean up - only known sex, known age, age 4-10,
+# stages developing - spent
+
+mature.weights <- dat %>%
+  dplyr::filter(maturity_table_3 %in% 2:5,
+         Age %in% 4:10,
+         sex.code %in% 1:2)
+
+# log transform weight
+mature.weights$log.weight <- log(mature.weights$weight)
+
+# check for NAs (age 7 below is plotting as no data)
+check <- is.na(mature.weights$log.weight)
+sum(check)
+
+dplyr::filter(mature.weights, Age == 7)
+mature.weights[which.min(mature.weights$log.weight),] # one value of weight = 0!!
+
+mature.weights <- mature.weights %>%
+  dplyr::filter(weight > 0)
+
+# need to scale weight by age and sex
+females <- mature.weights %>%
+  dplyr::filter(sex.code == 2)
+
+males <- mature.weights %>%
+  dplyr::filter(sex.code == 1)
+
+
+female.weights <- plyr::ddply(females, "Age", transform, sc.weight = scale(log.weight))
+
+male.weights <- plyr::ddply(males, "Age", transform, sc.weight = scale(log.weight))
+
+
+mature.weights <- rbind(females, males)
+
+# check
+ggplot(mature.weights, aes(log.weight, sc.weight, color = as.factor(sex.code))) +
+  geom_point() +
+  facet_wrap(~Age, scales = "free")
+
+ggplot(mature.weights, aes(sc.weight)) +
+  geom_histogram(fill = "grey", color = "black", bins = 40) +
+  facet_wrap(~sex.code, scales = "free", ncol = 1)
+
+# assign year class
+levels(mature.weights$year)
+mature.weights$year <- as.numeric(as.character(mature.weights$year))
+
+mature.weights$year.class <- mature.weights$year - mature.weights$Age
+
+# plot to check
+ggplot(mature.weights, aes(Age)) +
+  geom_histogram() +
+  facet_wrap(~year.class)
+
+# clean up
+all.dat <- mature.weights %>%
+  select(-DateTime, -Sex)
+
+# set factors
+all.dat$sex.code <- as.factor(all.dat$sex.code)
+all.dat$age.factor <- as.factor(all.dat$Age)
+all.dat$maturity_table_3 <- as.factor(all.dat$maturity_table_3)
+all.dat$cohort <- as.factor(all.dat$year.class)
+
+#lag annual sst------
+#load sst data
+sst_lagged <- read.csv("./data/western.goa.sst.csv")
+
+
+sst_lagged$prevyr_annual.wSST <- NA
+k<-1
+for(k in 1:length(sst_lagged$year)){
+  if(k>1){
+    sst_lagged$prevyr_annual.wSST[k] <- sst_lagged$annual.wSST[k-1] 
+  }
+} #works now join
+
+sstjoin <- sst_lagged[,c(1,5)]
+
+dat_lag <- left_join(all.dat, sstjoin)
+
+# try a combined model - separate smooths to each sex
+mod1 <- gamm4(sc.weight ~  s(prevyr_annual.wSST, by = age.factor, k=4) + maturity_table_3,
+                random=~(1|year/Haul) + (1|cohort), data=dat_lag) # model does not fit
+
+# save the model object
+saveRDS(mod1, "./output/acoustic_trawl_weight_mod1.rds")
+weight_mod1 <- readRDS("./output/acoustic_trawl_weight_mod1.rds")
+
+gam.check(weight_mod1$gam)
+plot(weight_mod1$gam)
+summary(weight_mod1$gam)
+anova(weight_mod1$gam)
+
+
+# try with random effect maturity:sex.code
+coBnoa <- gamm4(sc.weight ~  s(prevyr_annual.wSST, by = sex.code, k=4),
+                random=~(1|year/Haul) + (1|cohort) + (1|sex.code/maturity_table_3), data=dat_lag) # no dice
